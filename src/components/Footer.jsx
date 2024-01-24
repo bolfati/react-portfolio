a2eb2ad2618996a5d9fd85e2d353b5b711d29449
Bij <bijan.olfati@gmail.com>
2024-01-23 23:22:38 -0600

    adding content to home page, about page, and bootstrap stylizing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 export default function Footer() {
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
-   <div>Page Footer</div>
+   <div className="fixed-bottom text-center bg-dark text-primary h-15">
+    <a className="px-2" href="https://github.com/bolfati">Github Page</a>
+    </div>
   );
 }
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,6 +4,12 @@ export default function AboutPage() {
   return (
     <div className="container pt-4">
       <h1>About Bijan</h1>
+        <p>
+            Bijan is a full-stack software developer with a background in mechanical engineering and a passion for learning. 
+            Bijan received his CSWA and AutoCAD certification at the age of 17, reached managerial positions surpassing his peers, and has a strong emphasis on teamwork, honesty, and empathy.
+            Always striving to learn more, Bijan is currently enrolled in the University of Texas San Antonio's Full Stack Software Development Curriculum.
+            Never afraid to take leadership roles, Bijan has been a leader in many of his previous positions and is always looking to learn and grow while helping others do the same.
+        </p>
     </div>
   );
 }
