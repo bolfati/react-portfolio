e853b14cff63b572425a4388a68d15acc118d545
Bij <bijan.olfati@gmail.com>
2024-03-04 03:15:15 -0600

    updated text

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,7 +7,7 @@ export default function AboutPage() {
         <p>
             Bijan is a full-stack software developer with a background in mechanical engineering and a passion for learning. 
             Bijan received his CSWA and AutoCAD certification at the age of 17, reached managerial positions surpassing his peers, and has a strong emphasis on teamwork, honesty, and empathy.
-            Always striving to learn more, Bijan is currently enrolled in the University of Texas San Antonio's Full Stack Software Development Curriculum.
+            Always striving to learn more, Bijan is an Alumni of the University of Texas at San Antonio's Full Stack Software Development program.
             Never afraid to take leadership roles, Bijan has been a leader in many of his previous positions and is always looking to learn and grow while helping others do the same.
         </p>
         <p>
